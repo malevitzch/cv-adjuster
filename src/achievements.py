bd139db3bd77754a6831f89cefae0985a3faed1f
malevitzch <malevitzch@gmail.com>
2026-08-29 19:55:31 +0200

    Improvements to achievement processing

--- src/achievements.py
+++ src/achievements.py
@@ -1,19 +1,28 @@
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 from markitdown import MarkItDown
 
 md = MarkItDown()
 
 
-def summarize_achievements(source_path: Path, target_path: Path):
+def mdify_achievements(source_path: Path, target_path: Path):
     if source_path.is_file():
         result = md.convert(source_path)
         txt = result.markdown
         target_path.parent.mkdir(parents=True, exist_ok=True)
         name = target_path.stem + ".md"
         with open(target_path.with_name(name), "w") as output_file:
+            # TODO: check if it didn't fail, maybe log somewhere
             _ = output_file.write(txt)
 
     elif source_path.is_dir():
         for child in source_path.iterdir():
-            summarize_achievements(child, target_path / child.name)
+            mdify_achievements(child, target_path / child.name)
+
+
+def clean_achievements():
+    _ = load_dotenv()
+
+    openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
--- src/scripts/process_achievements.py
+++ src/scripts/process_achievements.py
@@ -1,12 +1,25 @@
-import sys
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
+from typing import cast
 
-from achievements import summarize_achievements
+from achievements import mdify_achievements
 
 
-def main():
-    args = sys.argv
-    path = args[1]
-    outpath = args[2]
+class Arguments(Namespace):
+    path: str
+    outpath: str
 
-    summarize_achievements(Path(path), Path(outpath))
+    def __init__(self) -> None:
+        super().__init__()
+        self.path = ""
+        self.outpath = ""
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    _ = parser.add_argument("path", help="The path to the source directory")
+    _ = parser.add_argument("outpath", help="The path to the output directory")
+
+    args = cast(Arguments, parser.parse_args())
+
+    mdify_achievements(Path(args.path), Path(args.outpath))
